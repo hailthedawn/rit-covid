ca4330977bab9438318c811b58b434de2c4691f1
Ketaki Tilak <kkt7778@rit.edu>
2020-09-03 16:26:31 -0400

    Update ritcovidassessment.py
    Resized browser window to avoid Element Click Intercepted error in Selenium.

--- ritcovidassessment.py
+++ ritcovidassessment.py
@@ -5,14 +5,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
 
-username = 'YOUR_USERNAME'
-password = 'YOUR_PASSWORD'
+options = Options()
+options.add_argument("window-size=1400,600")
 
-driver = webdriver.Chrome('./chromedriver.exe')  # or wherever the path to the chromedriver file is
+username = 'username'
+password = 'password'
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver.exe')  # or wherever the path to the chromedriver file is
 url = 'https://dailyhealth.rit.edu/assessment'
 driver.get(url)
-
+driver.set_window_size(1920, 1080)
 delay = 3  # seconds
 try:
     myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div[2]/form/div[1]/div/input')))
@@ -23,10 +27,10 @@ try:
     driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/form/div[2]/div/input').send_keys(password)
     driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/form/button').click()
     try:
-        myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[1]/div/div/div[1]/div[2]/div/div/a')))
+        myElem = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[1]/div/div/div[1]/div[2]/div/div/a')))
         driver.find_element_by_xpath('/html/body/div/div[1]/div/div/div[1]/div[2]/div/div/a').click()
         try:
-            myElem = WebDriverWait(driver,  delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[1]/div/div/div[1]/div[2]/div[1]/div[2]/div/div[2]')))
+            myElem = WebDriverWait(driver,  delay).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[1]/div/div/div[1]/div[2]/div[1]/div[2]/div/div[2]')))
             driver.find_element_by_xpath('/html/body/div/div[1]/div/div/div[1]/div[2]/div[1]/div[2]/div/div[2]').click()
             driver.close()
         except TimeoutException:
